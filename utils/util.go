1259ddf43355fb301ad418eb7e472e425bdcee0e
Jael24 <jael.dubey@heig-vd.ch>
2019-10-13 21:47:14 +0200

    Add utils to delete redundant code

---
+++ utils/util.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"bufio"
+	"github.com/Laykel/PRR-Lab1/protocol"
+	"log"
+	"strconv"
+	"strings"
+)
+
+// Parse message separate by a character
+func parseUdpMessage(s bufio.Scanner) uint64 {
+	var result uint64
+
+	tokens := strings.FieldsFunc(s.Text(), func(r rune) bool {
+		return r == protocol.Separator
+	})
+
+	// Get the message code
+	result, err := strconv.ParseUint(tokens[1], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
